3f54dedde1da09d45fa006ad3afccf88b9eb4bf9
karandeep4423 <karanhanju9696@gmail.com>
2025-02-16 12:21:05 +0100

    remove home

--- app/[lang]/layout.js
+++ app/[lang]/layout.js
@@ -5,15 +5,15 @@ export default async function LangLayout({ children, params }) {
   return <Providers lang={lang}>{children}</Providers>;
 }
 
-export async function generateMetadata({ params: { lang } }) {
-  // Load translations directly from JSON files
-  const translations = await import(
-    `@/public/locales/${lang}/metaData.json`
-  );
+// export async function generateMetadata({ params: { lang } }) {
+//   // Load translations directly from JSON files
+//   const translations = await import(
+//     `@/public/locales/${lang}/metaData.json`
+//   );
 
-  return {
-    title: translations?.metaData['home'].title,
-    description: translations?.metaData['home'].description,
-    keywords: translations?.metaData['home'].keywords,
-  };
-}
+//   return {
+//     title: translations?.metaData['home'].title,
+//     description: translations?.metaData['home'].description,
+//     keywords: translations?.metaData['home'].keywords,
+//   };
+// }
