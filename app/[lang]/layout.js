1ed11cbef1998f9b0e2f49965fde690ebad618e0
karandeep4423 <karanhanju9696@gmail.com>
2025-02-16 13:12:08 +0100

    home error

--- app/[lang]/layout.js
+++ app/[lang]/layout.js
@@ -6,17 +6,35 @@ export default async function LangLayout({ children, params }) {
 }
 
 export async function generateMetadata({ params: { lang } }) {
-  try {
-    const translations = await import(`@/public/locales/${lang}/metaData.json`);
+  const language = await lang;
+  if (!language) {
     return {
-      title: translations?.metaData['home']?.title || "Default Title",
-      description: translations?.metaData['home']?.description || "Default Description",
-      keywords: translations?.metaData['home']?.keywords || [],
-    };
-  } catch {
-    return {
-      title: "home - Default Title",
+      title: "Default Title",
       description: "Default Description",
+    };
+  } else if (language === "en") {
+    return {
+      title: "Digital Solutions for IT & Marketing | Your Agency Name",
+      description:
+        "Empowering businesses with innovative IT and marketing services. From web development and mobile app creation to SEO and digital campaigns, our experienced team delivers creative and scalable solutions tailored to your needs.",
+      keywords:
+        "IT services, digital marketing, web development, mobile app development, SEO, social media marketing, digital transformation, Your Agency Name",
+    };
+  } else if (language === "fr") {
+    return {
+      title: "Solutions Digitales pour IT & Marketing | Your Agency Name",
+      description:
+        "Nous dynamisons les entreprises grâce à des services IT et marketing innovants. De la création de sites web et d'applications mobiles au SEO et aux campagnes digitales, notre équipe expérimentée propose des solutions créatives et évolutives, parfaitement adaptées à vos besoins.",
+      keywords:
+        "services IT, marketing digital, développement web, création d'applications mobiles, SEO, marketing sur les réseaux sociaux, transformation digitale, Your Agency Name",
+    };
+  } else if (language === "de") {
+    return {
+      title: "Digitale Lösungen für IT & Marketing | Your Agency Name",
+      description:
+        "Wir unterstützen Unternehmen mit innovativen IT- und Marketingdienstleistungen. Von Webentwicklung und mobiler App-Entwicklung bis hin zu SEO und digitalem Marketing – unser erfahrenes Team liefert kreative, skalierbare Lösungen, die exakt auf Ihre Bedürfnisse zugeschnitten sind.",
+      keywords:
+        "IT-Dienstleistungen, digitales Marketing, Webentwicklung, mobile App-Entwicklung, SEO, Social Media Marketing, digitale Transformation, Your Agency Name",
     };
   }
 }
