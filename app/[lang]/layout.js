e0c1b9757820e8214a8a1981907f9c09a99173c4
karandeep4423 <karanhanju9696@gmail.com>
2025-03-22 09:19:20 +0100

    removed one layout

--- app/[lang]/layout.js
+++ app/[lang]/layout.js
@@ -1,9 +1,37 @@
 import { Providers } from "./providers";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+import Navbar from "@/components/Navbar/page";
+import Footer from "@/components/Footer/page";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
-export default async function LangLayout({ children, params }) {
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default async function RootLayout({ children, params }) {
+  // Get the language from params (passed from LangLayout)
   const paramData = await params;
-  const lang = paramData?.lang;
-  return <Providers lang={lang}>{children}</Providers>;
+  const lang = paramData?.lang || 'fr';
+  return (
+    <html lang={lang}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ToastContainer autoClose={false} />
+        <Navbar />
+        <Providers lang={lang}>{children}</Providers>;
+        <Footer />
+      </body>
+    </html>
+  );
 }
 
 export async function generateMetadata({ params }) {
